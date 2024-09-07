646c84d537f83a27768a9c3882dbacc3416bede6
spexzee <abub5979@gmail.com>
2024-09-07 17:31:47 +0530

    some changes

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -1,6 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import L from 'leaflet'; // Leaflet for icon customization
+
+// Import marker images
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+// Fix marker icons not showing after deployment
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
 
 const MapComponent = ({ roomId, socket, location }) => {
   const [userLocations, setUserLocations] = useState([]);
